3050995bbda46c24a58a6b83256f29d6418ae7b2
Zachary E. Graber <zegraber@iu.edu>
2022-01-27 23:33:15 -0500

    trying out different error outputs

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -21,7 +21,7 @@ shellcmd xsh_prodcons(int nargs, char *args[]) {
 		count = atoi(args[1]);
 	}
 	else if (nargs > 2) {
-		fprintf(stderr, "Syntax: run prodcons \[counter\]\n");
+		printf("Syntax: run prodcons \\[counter\\]\n");
 		return(1);
 	}
 
